51de2bb4e27c7f07895cc91d68ff5d0e6daab295
Barto <bartoszhalubiec@gmail.com>
2024-10-02 18:30:46 +0200

    noise generator

---
+++ Source/NoiseGenerator.h
@@ -0,0 +1,22 @@
+/** @ Noise Generator - based on pseudo random number generator 
+*/
+
+#pragma once
+
+class NoiseGenerator
+{
+public:
+	void reset()
+	{
+		noiseSeed = 22222;
+	}
+
+	float nextValue()
+	{
+		noiseSeed = noiseSeed * 196314165 + 907633515;
+		int temp = int (noiseSeed >> 7) - 16777216;
+		return float(temp) / 16777216.0f;
+	}
+private:
+	unsigned int noiseSeed;
+};
